76b1c9fe75e985d941c2778b8054f317aef9b07f
Jakub Dolata <jdolata5@gmail.com>
2022-03-15 21:07:55 +0100

    add search

--- components/Map/index.js
+++ components/Map/index.js
@@ -7,23 +7,147 @@ export default function Map() {
 
   useEffect(() => {
     const loader = new Loader({
-      apiKey: process.env.MAPS_KEY,
-      version: 'weekly'
+      apiKey: process.env.NEXT_PUBLIC_MAPS_KEY,
+      version: 'weekly',
+      libraries: ['places']
     });
+
+    const myStyles = [
+      {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{ visibility: 'off' }]
+      },
+      //metro
+      {
+        featureType: 'administrative',
+        elementType: 'geometry',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'poi',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'road',
+        stylers: [
+          {
+            visibility: 'on'
+          }
+        ]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.icon',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'transit',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      }
+    ];
 
     const mapOptions = {
       center: {
-        lat: 0,
-        lng: 0
+        lat: 40.727505,
+        lng: -73.9389489
       },
-      zoom: 4
+      zoom: 12,
+      styles: myStyles
     };
 
     let map;
     loader.load().then(() => {
       const google = window.google;
       map = new google.maps.Map(googleMap.current, mapOptions);
+
+      // SEARCH BOX -----------------
+      // Create the search box and link it to the UI element.
+      const input = document.getElementById('pac-input');
+      const searchBox = new google.maps.places.SearchBox(input);
+
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      // Bias the SearchBox results towards current map's viewport.
+      map.addListener('bounds_changed', () => {
+        searchBox.setBounds(map.getBounds());
+      });
+
+      let markers = [];
+
+      // Listen for the event fired when the user selects a prediction and retrieve
+      // more details for that place.
+      searchBox.addListener('places_changed', () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+          return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach((marker) => {
+          marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach((place) => {
+          if (!place.geometry || !place.geometry.location) {
+            console.log('Returned place contains no geometry');
+            return;
+          }
+
+          const icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+          // Create a marker for each place.
+          markers.push(
+            new google.maps.Marker({
+              map,
+              icon,
+              title: place.name,
+              position: place.geometry.location
+            })
+          );
+          if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+          } else {
+            bounds.extend(place.geometry.location);
+          }
+        });
+        map.fitBounds(bounds);
+      });
     });
   });
-  return <div className={styles.map} id="map" ref={googleMap}></div>;
+  return (
+    <>
+      <input id="pac-input" className="controls" type="text" placeholder="Search Box" />
+      <button className="btn btn-primary mr-1">Metro</button>
+      <button className="btn btn-primary mr-1">Poi</button>
+      <div className={styles.map} id="map" ref={googleMap}></div>
+    </>
+  );
 }
