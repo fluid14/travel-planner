0c39b596d9d0643c00193b3c35273d6a2383f9d3
Jakub Dolata <jdolata5@gmail.com>
2022-04-03 14:18:27 +0200

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require('path');
+const withPWA = require('next-pwa');
 
-const nextConfig = {
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-}
-
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true
+  }
+});
